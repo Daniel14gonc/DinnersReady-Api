1d1c3de076c0541b08d17218d3a29f5731f83f16
Daniel <gon20293@uvg.edu.gt>
2022-09-01 23:16:24 -0600

    cambio en comando de shell script

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,10 @@ sudo rm -rf node_modules package-lock.json
 sudo npm install
 
 echo "Run new PM2 action"
-pm2 restart index
+
+if ! type pm2 > /dev/null
+then
+    sudo npm install -g pm2
+else
+    pm2 restart index
+fi
