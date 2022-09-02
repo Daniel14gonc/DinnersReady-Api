75bcff50f7c8ef74b786073ea3178365b90fb1ed
Daniel <gon20293@uvg.edu.gt>
2022-09-02 12:43:17 -0600

    cambio en shell script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,10 @@ echo "Install app dependencies"
 sudo rm -rf node_modules package-lock.json
 sudo npm install
 
+
+echo "NVM fix"
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+
 echo "Run new PM2 action"
-pm2 list
+npm deploy
