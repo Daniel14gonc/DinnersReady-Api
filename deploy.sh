6ab414ede164ac8c186000a03bcb5bf6df8cf3b8
Daniel <gon20293@uvg.edu.gt>
2022-09-01 23:02:36 -0600

    cambio en comando de shell script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,5 @@ echo "Install app dependencies"
 sudo rm -rf node_modules package-lock.json
 sudo npm install
 
-echo "Build your app"
-sudo npm run dev
-
 echo "Run new PM2 action"
-sudo pm2 restart --name "Dinners"
+sudo pm2 restart --name "index"
