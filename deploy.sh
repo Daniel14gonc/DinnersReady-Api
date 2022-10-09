8d6e2455ddb940591788c86eef7990ee5ef09223
Juan Angel Carrera <angelitocarrera@gmail.com>
2022-10-09 14:04:10 -0600

    Pm2 Prubea

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,5 @@ echo "Install app dependencies"
 sudo rm -rf node_modules package-lock.json
 sudo npm install
 
-
-echo "NVM fix"
-export NVM_DIR=~/.nvm
-source ~/.nvm/nvm.sh
-
 echo "Run new PM2 action"
-npm run deploy
+pm2 restart PruebaJack
